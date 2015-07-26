4dee477f37c08680e75246c8ff5fe12abebf8996
joycedelatorre <joycedelatorre87@gmail.com>
2015-07-25 23:45:30 -0700

    add ruby practice palindrome directions

--- cb_practice/letter_capitalize.rb
+++ cb_practice/letter_capitalize.rb
@@ -1,4 +1,16 @@
 #LetterCapitalize :Using the Ruby language, have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. Words will be separated by only one space.
+
+### solution 1
+
+def letter_capitalize (str)
+  puts str.split.map(&:capitalize).join(' ')
+end
+
+letter_capitalize("hello world")
+
+
+
+### solution 2
 def LetterCapitalize(str)
   tmp = ""
   array= str.split
@@ -10,3 +22,18 @@ return tmp
 
 end
  puts LetterCapitalize("the quick brown fox jumps over the lazy dog.")
+
+
+ ### solution 3
+def LetterCapitalize(str)
+   array1 = str.split(" ")
+   captl = ""
+   array1.each{|word|
+     captl = captl + " " + word.capitalize
+      }
+    new_word = captl
+   puts new_word
+
+end
+
+LetterCapitalize("hello world greetings to earthlings")
