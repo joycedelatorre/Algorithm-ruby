16b135d15450b615fbba8f3acf29652016e511e6
joycedelatorre <joycedelatorre87@gmail.com>
2015-07-26 20:20:35 -0700

    add initial step

--- cb_practice/vowel_count.rb
+++ cb_practice/vowel_count.rb
@@ -1,1 +1,8 @@
  # Using the Ruby language, have the function VowelCount(str) take the str string parameter being passed and return the number of vowels the string contains (ie. "All cows eat grass" would return 5). Do not count y as a vowel for this challenge.
+
+ def VowelCount(str)
+    array = str.split("")
+    p array
+ end
+
+ VowelCount("All cows eat grass")
