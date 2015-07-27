afb8786f1f469506bb74543b48c016a80a01bac9
joycedelatorre <joycedelatorre87@gmail.com>
2015-07-27 10:50:38 -0700

    add word count solution

--- cb_practice/word_count.rb
+++ cb_practice/word_count.rb
@@ -1,1 +1,9 @@
  # Using the Ruby language, have the function WordCount(str) take the str string parameter being passed and return the number of words the string contains (ie. "Never eat shredded wheat" would return 4). Words will be separated by single spaces.
+
+ def WordCount(str)
+  word_ary = str.split(" ")
+  p word_ary.length
+
+ end
+
+ WordCount("Never eat shredded wheat" )
