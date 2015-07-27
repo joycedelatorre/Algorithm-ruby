72298d45c8ad45ba9143dcebfd743e0486e139e8
joycedelatorre <joycedelatorre87@gmail.com>
2015-07-26 21:25:37 -0700

    pushed academic_mad_lib

---
+++ academic_mad_lib/my_solution.rb
@@ -0,0 +1,9 @@
+filipino_history=['Jose Rizal', 'national hero', 1898, 'Doctor', 'Bagumbayan']
+health_care=['skin', 'largest organ of the body', 'epidermis', 'dermis', 'hypodermis', 20, 'protection']
+IT=['ruby', 'javascript', 'front-end', 'jquery', 'rails']
+
+
+
+def essay_writer(title, topic, date, thesis_statment, pronoun)
+
+end
