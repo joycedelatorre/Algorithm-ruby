5b67b3420d4a40d4196aaf631b2c20f67d1176f8
joycedelatorre <joycedelatorre87@gmail.com>
2015-07-28 11:06:17 -0700

    add binary array

--- cb_practice/binary_converter.rb
+++ cb_practice/binary_converter.rb
@@ -3,24 +3,55 @@
     # Input = "011"Output = "3"
 
 
-def BinaryConverter(str)
-  ary_reversed_binary = str.reverse.split("")
-  binary_container =[]
-  binary = 0
-  ary_reversed_binary.each_with_index do|num, index|
-    if (index == 0)&&(num == "1")
-      binary += 1
-    end
-    if (index == 1)&&(num == "1")
-      binary = binary +=2
-    end
-    if (index == 2)&&(num == "1")
-      binary = binary **2
-    end
-    binary_container.push(binary)
-  end
-  p binary_container
+# def BinaryConverter(str)
+#   ary_reversed_binary = str.reverse.split("")
+#   binary_container =[]
+#   binary = 0
+#   ary_reversed_binary.each_with_index do|num, index|
+#     if (index == 0)&&(num == "1")
+#       binary += 1
+#     end
+#     if (index == 1)&&(num == "1")
+#       binary = binary +=2
+#     end
+#     if (index == 2)&&(num == "1")
+#       binary = binary **2
+#     end
+#     binary_container.push(binary)
+#   end
+#   p binary_container
 
+# end
+
+# BinaryConverter("100101")
+
+
+# def pow(base, exponent)
+#   result = 1
+#   i = 1
+#   ary =[1]
+#   while i <= exponent
+#     result = result * base
+#     i += 1
+#     ary.push(result)
+#   end
+#   p ary
+
+# end
+# pow(2, str.length)
+
+def BinaryConverter(str)
+  reversed_binary = str.reverse.split("")
+
+  i = 0
+  temp_binary = 1
+  ary_of_binaries = [1]
+  while i <= (reversed_binary.length-2)
+    temp_binary = temp_binary * 2
+    i+=1
+    ary_of_binaries.push(temp_binary)
+  end
+  p ary_of_binaries
 end
 
 BinaryConverter("100101")
